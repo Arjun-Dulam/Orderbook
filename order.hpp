7fb7a533191add901f80e1bc522552c5bde8e524
adulam <adulam3@gatech.edu>
2025-12-25 03:16:58 -0500

    struct defs

--- order.hpp
+++ order.hpp
@@ -1,8 +1,23 @@
-//
-// Created by Arjun Dulam on 12/23/25.
-//
+#pragma once
+#include <cstdint>
 
-#ifndef MULTI_STOCK_EXCHANGE_ORDER_H
-#define MULTI_STOCK_EXCHANGE_ORDER_H
+enum Side {
+    Buy,
+    Sell
+};
 
-#endif //MULTI_STOCK_EXCHANGE_ORDER_H
+struct Order {
+    uint32_t order_id;
+    Side side;
+    int32_t price; // Price is stored as cents, choosing to have two decimal places of precision.
+    uint32_t quantity;
+    uint64_t timestamp;
+};
+
+struct Trade {
+    uint32_t trade_id;
+    int32_t price;
+    uint32_t quantity;
+    uint32_t buy_order_id;
+    uint32_t sell_order_id;
+};
