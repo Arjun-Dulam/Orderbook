64509fd628cd9e4e19a153e0a0a264b2e147ea8b
adulam <adulam3@gatech.edu>
2025-12-25 04:08:39 -0500

    testing

--- test.cpp
+++ test.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
 #include "order.hpp"
 
-int main {
-}
+int main() {
+    Order new_order = Order{1, Buy, 10, 10, 1};
+    Order other_order = Order{2, Sell, 100, 100, 20};
+
+    std::cout << new_order.order_id << side_to_string(new_order.side) << new_order.price << new_order.quantity << new_order.timestamp << '\n';
+    std::cout << other_order.order_id << side_to_string(other_order.side) << other_order.price << other_order.quantity << other_order.timestamp << '\n';
+
+    Trade new_trade = Trade{1, 100, 100, new_order.order_id, other_order.order_id};
+
+    std::cout << new_trade.trade_id << new_trade.price << new_trade.quantity << new_trade.buy_order_id << new_trade.sell_order_id << '\n';
+
+    return 0;
+};
 
