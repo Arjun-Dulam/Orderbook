7a1b3a0b754b89fde42ae27580b195fe761a49d2
adulam <adulam3@gatech.edu>
2025-12-30 01:22:46 -0500

    finish add method

--- src/orderbook.cpp
+++ src/orderbook.cpp
@@ -1,9 +1,24 @@
 #include "orderbook.hpp"
 
-OrderBook::OrderBook(): next_timestamp ( 0 ) {}
+OrderBook::OrderBook(): next_timestamp (0)  {}
 
 std::vector<Trade> OrderBook::add_order(const Order &order) {
-    // TODO
+    Order new_order = order;
+    new_order.timestamp = next_timestamp++;
+
+    std::map<int32_t, std::vector<Order>> *target_map;
+    if (new_order.side == Side::Buy) {
+        target_map = &bids;
+    } else {
+        target_map = &asks;
+    }
+
+    (*target_map)[new_order.price].push_back(new_order);
+
+    size_t index = size((*target_map)[new_order.price]) - 1;
+    OrderLocation order_location{new_order.side, new_order.price, index};
+    order_lookup[new_order.order_id] = order_location;
+
     return {};
 }
 
