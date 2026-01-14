353c75b243e1abe62c6c9383f1f4dfcad7155e40
adulam <arjunkumardulam@gmail.com>
2026-01-14 03:06:21 -0500

    mixed workload test done

--- benchmarks/orderbook_bench.cpp
+++ benchmarks/orderbook_bench.cpp
@@ -5,12 +5,10 @@
 #include "../include/orderbook.hpp"
 #include "../include/order.hpp"
 
-#define NUM_ORDERS 15000000
-
 static void BM_AddOrder_No_Match(benchmark::State &state) {
     OrderBook order_book;
     OrderGenerator order_gen(MarketConfig{});
-    const size_t num_orders = NUM_ORDERS;
+    const size_t num_orders = 15000000;
 
     std::vector<Order> orders_to_add;
     orders_to_add.reserve(num_orders);
@@ -37,17 +35,17 @@ static void BM_AddOrder_No_Match(benchmark::State &state) {
 }
 
 BENCHMARK(BM_AddOrder_No_Match)
-    -> Arg(0) // cold start
-    -> Arg(1000)
-    -> Arg(10000)
-    -> Arg(100000)
-    -> Arg(1000000)
-    -> Arg(NUM_ORDERS);
+-> Arg(0) // cold start
+-> Arg(1000)
+-> Arg(10000)
+-> Arg(100000)
+-> Arg(1000000)
+-> Arg(15000000);
 
 static void BM_AddOrder_Latency(benchmark::State &state) {
     OrderBook order_book;
     OrderGenerator order_gen(MarketConfig{});
-    const size_t num_orders = NUM_ORDERS;
+    const size_t num_orders = 15000000;
 
     std::vector<double> latencies;
     latencies.reserve(num_orders);
@@ -85,12 +83,12 @@ static void BM_AddOrder_Latency(benchmark::State &state) {
 }
 
 BENCHMARK(BM_AddOrder_Latency)
-    -> Arg(0)
-    -> Arg(1000)
-    -> Arg(10000)
-    -> Arg(100000)
-    -> Arg(1000000)
-    -> Arg(NUM_ORDERS);
+-> Arg(0)
+-> Arg(1000)
+-> Arg(10000)
+-> Arg(100000)
+-> Arg(1000000)
+-> Arg(15000000);
 
 
 static void BM_RemoveOrder_VaryDepth(benchmark::State &state) {
@@ -129,34 +127,83 @@ static void BM_RemoveOrder_VaryDepth(benchmark::State &state) {
 }
 
 BENCHMARK(BM_RemoveOrder_VaryDepth)
-    -> Arg(10000)
-    -> Arg(100000)
-    -> Arg(1000000)
-    -> Arg(10000000)
-    -> Arg(NUM_ORDERS);
+-> Arg(0)
+-> Arg(1000)
+-> Arg(10000)
+-> Arg(100000)
+-> Arg(1000000)
+-> Arg(15000000);
 
 static void BM_MixedWorkload(benchmark::State &state) {
     OrderBook order_book;
-    OrderGenerator order_gen(MarketConfig{});
-    const size_t num_orders = NUM_ORDERS;
+    MarketConfig cfg;
+    OrderGenerator order_gen(cfg);
+    const size_t num_orders = 10000000;
 
     std::vector<Order> orders_to_add;
     orders_to_add.reserve(num_orders);
 
-    for (size_t i = 0; i < num_orders; i++) {
-        orders_to_add.push_back(order_gen.generate_order());
+    std::vector<uint32_t> orders_to_remove;
+    orders_to_remove.reserve(num_orders);
+
+    std::vector<bool> should_cancel_vec;
+    should_cancel_vec.reserve(num_orders);
+
+    for (size_t i = 0; i < num_orders; i++) { // Order Stream
+        Order new_order = order_gen.generate_order();
+
+        if (new_order.side == Side::Buy) {new_order.price -= 100;}
+        else {new_order.price += 100;}
+
+        orders_to_add.push_back(new_order);
     }
 
-    for (int i = 0; i < state.range(0); i++) {
+    for (size_t i = 0; i < state.range(0); i++) { // Prepopulating Orderbook
         Order new_order = order_gen.generate_order();
+
+        if (new_order.side == Side::Buy) {new_order.price -= 100;}
+        else {new_order.price += 100;}
+
         order_book.add_order(new_order);
     }
 
-    for (auto _ : state) {
-
+    for (size_t i = 0; i < num_orders; i++) {
+        should_cancel_vec.push_back(order_gen.should_cancel());
     }
 
+    OrderBook backup_order_book = order_book;
+    size_t orderIdx = 0;
+
+    for (auto _ : state) {
+        if (orderIdx == num_orders - 1) {
+            state.PauseTiming();
+            orderIdx = 0;
+            order_book = backup_order_book;
+            state.ResumeTiming();
+        }
+
+        Order &new_order = orders_to_add[orderIdx];
+        order_book.add_order(new_order);
+
+        if (new_order.quantity > 0) {
+            orders_to_remove.push_back(new_order.get_order_id());
+        }
+
+        if (should_cancel_vec[orderIdx] && !orders_to_remove.empty()) {
+            size_t removal_index =  orders_to_remove.size() > 100 ? orders_to_remove.size() - 100 : 0;
+            order_book.remove_order(orders_to_remove[removal_index]);
+        }
+        orderIdx++;
+    }
+    state.SetItemsProcessed(state.iterations());
 }
 
+BENCHMARK(BM_MixedWorkload)
+-> Arg(0)
+-> Arg(1000)
+-> Arg(10000)
+-> Arg(100000)
+-> Arg(1000000)
+-> Arg(15000000);
 
 BENCHMARK_MAIN();
